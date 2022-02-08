7ba6957bf788bd415eb95756d72c3cde19edee12
alinabezux <97479011+alinabezux@users.noreply.github.com>
2022-02-08 02:15:01 +0200

    lesson 1

--- lesson1/hw1.js
+++ lesson1/hw1.js
@@ -1,2 +1,164 @@
 //- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 // Вивести кожну змінну за допомогою: console.log , alert, document.write
+/*
+let a = 'hello'
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let e = 1
+let f = 10
+let g = -99
+let h = 123
+let i = 3.14
+let j = 2.7
+let k = 16
+let l = true
+let m = false
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
+alert(j);
+alert(k);
+alert(l);
+alert(m);
+document.write(`a`);
+document.write(`b`);
+document.write(`c`);
+document.write(`d`);
+document.write(`e`);
+document.write(`f`);
+document.write('g');
+document.write('h');
+document.write('i');
+document.write('j');
+document.write('k');
+document.write('l');
+document.write('m');
+*/
+
+//- Переприсвоїти кожній змінній з завдання значення на довільне.
+//    Вивести кожну змінну за допомогою: console.log , alert, document.write
+/*
+ a = 15;
+ b = 'cool';
+ c = -20;
+ d = false;
+ e = 101;
+ f = true;
+ g = 9;
+ h = 13.56;
+ i = 3.14;
+ j = 3.8;
+ k = 0;
+ l = 'mamba';
+ m = 'hahaha';
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
+alert(j);
+alert(k);
+alert(l);
+alert(m);
+document.write(`a`);
+document.write(`b`);
+document.write(`c`);
+document.write(`d`);
+document.write(`e`);
+document.write(`f`);
+document.write('g');
+document.write('h');
+document.write('i');
+document.write('j');
+document.write('k');
+document.write('l');
+document.write('m');
+*/
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person.
+// (Не об'єкт)
+/*let firstName = 'Alina';
+let middleName ='Bezukh';
+let lastName ='Yuriivna';
+let person = [firstName,middleName,lastName];*/
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+  /* let name =  prompt('Введіть ваше імя',' імя');
+   let surname = prompt('Введіть ваше імя по-батькові','імя по-батькові');
+   let years = prompt('Введіть ваш вік','вік');
+   document.write(`<div>Вітаю, ${name} ${surname}.Тобі ${years} років.</div>`);*/
+
+//  За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+    /*   let a = 100;
+   let b = '100';
+   let c = true;
+   console.log(typeof a);
+   console.log(typeof b);
+   console.log(typeof c);*/
+
+// - Визначити відповідний оператор в виразах ,щоб вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
+  /* console.log(5 < 6); //-> true
+   console.log(5 >= 6);// -> false
+   console.log(5 === 6 );//-> false
+   console.log(5 > 6) ;// -> false
+   console.log(10 == 10); //-> true
+   console.log(10 === 10);// -> true
+   console.log(10 < 10); //-> false
+   console.log(10 > 10 );//-> false
+   console.log(10 !== 10 );//-> false
+   console.log(123 === '123'); //-> false
+   console.log(123 !== '123') ; //-> true*/
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+   /* let str = "20";
+    let a = 5;
+    document.write(str + a + "<br/>"); //205
+    document.write(str - a + "<br/>");// 15
+    document.write(str * "2" + "<br/>"); //40
+    document.write(str / 2 + "<br/>");// 10
+*/
+
+
+
+
+
