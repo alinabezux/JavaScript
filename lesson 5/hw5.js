1c97198c357a67f9a371d0b1797178731b5a7902
alinabezux <97479011+alinabezux@users.noreply.github.com>
2022-02-26 21:17:24 +0200

    lesson 5

--- lesson 5/hw5.js
+++ lesson 5/hw5.js
@@ -20,9 +20,57 @@
 // document.write(squareCylinder(5, 4));
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
+// let arr = [1, 2, 3, 'hello', true];
+// const foo = (array) => {
+//     for (let i = 0; i < array.length; i++) {
+//         document.write(array[i]);
+//     }
+// }
+// foo(arr);
+
 // - створити функцію яка  створює параграф з текстом. Текст задати через аргумент
+// const func = (text) => {
+//     document.write(`<p>${text}</p>`);
+// }
+// func('sdfghjkl');
+
 // - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// const createUl = (text) => {
+//     document.write(`<ul>`);
+//         document.write(`<li>${text}</li>`);
+//         document.write(`<li>${text}</li>`);
+//         document.write(`<li>${text}</li>`);
+//     document.write(`</ul>`);
+// }
+// createUl(`dfghjk`);
+
 // - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+// const createUl = (text, number) => {
+//     document.write(`<ul>`);
+//     for (let i = 0; i < number; i++) {
+//         document.write(`<li>${text}</li>`);
+//     }
+//     document.write(`</ul>`);
+// }
+// createUl(`dfghjk`, 3);
+
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-//
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// let arr = [1, 63, true, 'hello'];
+// const foo = (array) => {
+//     document.write(`<ol>`);
+//     for (let i = 0; i < array.length; i++) {
+//         document.write(`<li>${array[i]}</li>`);
+//     }
+//     document.write(`</ol>`);
+// }
+// foo(arr);
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.
+// Для кожного об'єкту окремий блок.
+// let arr = [{id: 1, name: 'Alina', age: 18}, {id: 2, name: 'Misha', age: 11}, {id: 3, name: 'Yuriy', age: 41}];
+// const func = (array) => {
+//     for (const arrayElement of array) {
+//         document.write(`<div>${arrayElement.id}. ${arrayElement.name} ${arrayElement.age}</div>`);
+//     }
+// }
+// func(arr);
